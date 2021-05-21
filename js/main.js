f8c9cc29e251744bedb6aa4ba27b8aa43a52400c
habib <habibdev96@gmail.com>
2021-05-20 21:03:52 -0600

    created preloader

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 import validateEmail from "./validateEmail.js";
+import preloader from "./preloader.js";
 
 const init = () => {
+  preloader();
   validateEmail();
 };
 
---
+++ js/preloader.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const preloader = () => {
+  const preloaderEl = document.querySelector(".preloader");
+
+  window.addEventListener("load", function () {
+    preloaderEl.classList.add("hide-preloader");
+  });
+};
+
+export default preloader;
